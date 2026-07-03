5f93491a4cf6f3a5697a0e39fab1e2e1f1058e67
Nileshnhe <nehenilesh111@gmail.com>
2026-07-03 15:09:03 +0530

    feat: add onboarding functionality and update auth routes to include onboarding endpoint

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -37,11 +37,11 @@ export const signup = async (req, res) => {
 
         try {
             await upsertStreamUser({
-            id: newUser._id.toString(),
-            name: newUser.fullName,
-            image: newUser.profilePic || "",
-        });
-        console.log(`Stream user created for ${newUser.fullName}`);
+                id: newUser._id.toString(),
+                name: newUser.fullName,
+                image: newUser.profilePic || "",
+            });
+            console.log(`Stream user created for ${newUser.fullName}`);
         } catch (error) {
             console.log("Error creating Stream user:", error);
         }
@@ -117,3 +117,48 @@ export const logout = async (req, res) => {
 
     }
 };
+
+export async function onboard(req, res) {
+    try {
+        const userId = req.user._id;
+
+        const { fullName, bio, nativeLanguage, learningLanguage, location } = req.body;
+
+        if (!fullName || !bio || !nativeLanguage || !learningLanguage || !location) {
+
+            return res.status(400).json({
+                message: "All fields are required",
+                missingFields: [
+                    !fullName && "fullName",
+                    !bio && "bio",
+                    !nativeLanguage && "nativeLanguage",
+                    !learningLanguage && "learningLanguage",
+                    !location && "location",
+                ].filter(Boolean),
+            });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            ...req.body,
+            isOnboarded: true,
+        }, { new: true });
+
+        if (!updatedUser) return res.status(404).json({ message: "User not found" });
+
+        try {
+            await upsertStreamUser({
+                id: updatedUser._id.toString(),
+                name: updatedUser.fullName,
+                image: updatedUser.profilePic || ""
+            });
+            console.log(`Stream user updated after onboarding for ${updatedUser.fullName}`);
+        } catch (streamError) {
+            console.log("Error updating Stream user during onboarding:", streamError.message);
+        }
+
+        res.status(200).json({ success: true, user: updatedUser });
+    } catch (error) {
+        console.error("Onboarding error:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,18 +1,17 @@
 import { Router } from "express";
-import { login, logout, me, signup } from "../controller/auth.controller.js";
-
+import { login, logout, me, onboard, signup } from "../controller/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 
 const authRoute = Router();
 
 
 authRoute.post("/signup", signup);
-
 authRoute.post("/login", login);
-
-authRoute.get("/me", me);
-
+authRoute.get("/me", protectRoute, me);
 authRoute.post("/logout", logout);
+
+authRoute.post("/onboarding", protectRoute, onboard)
 
 
 export default authRoute;
