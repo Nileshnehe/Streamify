c4e06ce4f6bec8101c623d6429a0b237e5d3004b
Nileshnhe <nehenilesh111@gmail.com>
2026-07-04 11:23:20 +0530

    config: setup CORS for frontend communication

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 import authRoute from "./routes/auth.routes.js";
 import userRoute from "./routes/user.routes.js";
 import chatRoute from "./routes/chat.route.js";
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());    
+
 
 
 //health check api
